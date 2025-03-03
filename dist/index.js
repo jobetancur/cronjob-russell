81cb11ff140e5d79987f4abdd6697ff205f597c3
Alejandro <alejobetancur2@gmail.com>
2025-03-03 11:10:06 -0500

    production config

--- dist/index.js
+++ dist/index.js
@@ -12,33 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const node_schedule_1 = __importDefault(require("node-schedule"));
 const checkConversations_1 = require("./utils/checkConversations");
-const app = (0, express_1.default)();
-app.options('*', (0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-const PORT = 3021;
-const allowedOrigins = ['http://localhost:5173', 'https://ultim-admin-dashboard.web.app', 'https://ultim-admin-dashboard.firebaseapp.com', 'https://dashboard.ultim.pro', 'https://ultim.pro', 'https://trasnferchat-1336.twil.io/transferChat', 'https://trasnferchat-1336.twil.io', 'https://ultim.pro/dashboard/carestream/chat-carestream'];
-app.use((0, cors_1.default)({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
-app.options('*', (0, cors_1.default)());
-// schedule.scheduleJob('*/1 * * * *', checkConversations); // Cada minuto
-// schedule.scheduleJob("0 * * * *", checkConversations); // Cada hora
-node_schedule_1.default.scheduleJob("*/1 * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
+// Ejecutar el cronjob cada 1 minuto
+node_schedule_1.default.scheduleJob("0 * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Running cron job...");
     yield (0, checkConversations_1.checkConversations)();
 }));
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+console.log("🚀 Cronjob iniciado y ejecutándose en segundo plano...");
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-import express from 'express';
-import cors from 'cors';
 import schedule from "node-schedule";
 import { checkConversations } from './utils/checkConversations';
 
-const app = express();
-app.options('*', cors()); 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-const PORT = 3021;
-
-const allowedOrigins = ['http://localhost:5173', 'https://ultim-admin-dashboard.web.app', 'https://ultim-admin-dashboard.firebaseapp.com', 'https://dashboard.ultim.pro', 'https://ultim.pro', 'https://trasnferchat-1336.twil.io/transferChat', 'https://trasnferchat-1336.twil.io', 'https://ultim.pro/dashboard/carestream/chat-carestream'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-
-app.options('*', cors());
-
-// schedule.scheduleJob('*/1 * * * *', checkConversations); // Cada minuto
-// schedule.scheduleJob("0 * * * *", checkConversations); // Cada hora
-
-schedule.scheduleJob("*/1 * * * *", async () => {
+// Ejecutar el cronjob cada 1 minuto
+schedule.scheduleJob("0 * * * *", async () => {
   console.log("Running cron job...");
   await checkConversations();
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+console.log("🚀 Cronjob iniciado y ejecutándose en segundo plano...");

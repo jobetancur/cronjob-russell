d84f26a412a0c2217c44f335a9e8f7f2f521d969
Alejandro <alejobetancur2@gmail.com>
2025-04-23 14:08:02 -0500

    production cron job config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import schedule from "node-schedule";
 import { checkConversations } from './utils/checkConversations';
 
-// schedule.scheduleJob("0 * * * *", async () => {
-//   console.log("Running cron job...");
-//   await checkConversations();
-// });
-
-// Ejecutar el cronjob cada minuto (para pruebas)
-schedule.scheduleJob("* * * * *", async () => {
+schedule.scheduleJob("0 * * * *", async () => {
   console.log("Running cron job...");
   await checkConversations();
 });
+
+//! Ejecutar el cronjob cada minuto (para pruebas)
+// schedule.scheduleJob("* * * * *", async () => {
+//   console.log("Running cron job...");
+//   await checkConversations();
+// });
 
 console.log("🚀 Cronjob iniciado y ejecutándose en segundo plano...");
